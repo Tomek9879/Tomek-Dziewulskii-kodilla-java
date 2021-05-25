1966c24f6d40b05dac1921d1afcaa9780de7e644
Tomek <tomek-dziewulski@wp.pl>
2021-05-25 16:39:21 +0200

    6.2 Zadanie:Pierwszy test jednostkowy

--- kodilla -testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla -testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,27 @@
 package com.kodilla.testing;
-
+import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
 
     public static void main (String [] args ){
 
-        System.out.println("Moduł 6 Wprowadzenie do testowania oprogramowania ");
+        Calculator calculator = new Calculator();
+        int liczba1=3;
+        int liczba2=4;
+        int liczba3=10;
+        int liczba4=4;
+        // test dodawania liczb
+        int wynik = calculator.add(liczba1,liczba2);
+        if(wynik ==(liczba1+liczba2))
+            System.out.println("Wynik  dodawania liczb:"+liczba1+" i "+liczba2+" jest prawidłowy i wynosi "+wynik);
+        else
+            System.out.println("Wynik  dodawania liczb:"+liczba1+" i "+liczba2+" jest nieprawidłowy i wynosi "+wynik);
+
+        // test odejmowania liczb
+        wynik=calculator.subtract (liczba3,liczba4);
+        if(wynik==(liczba3-liczba4))
+            System.out.println("Wynik  odejmowania liczb:"+liczba3+" i "+liczba4+" jest prawidłowy i wynosi "+wynik);
+        else
+            System.out.println("Wynik  odejmowania liczb:"+liczba3+" i "+liczba4+" jest nieprawidłowy i wynosi "+wynik);
 
 
 
---
+++ kodilla -testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+   public int add (int liczba1, int liczba2){
+        return liczba1 +liczba2;
+    }
+
+    public int subtract (int liczba1, int liczba2){
+        return liczba1-liczba2;
+    }
+
+}
