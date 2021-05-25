49f57c9331330b28af3f36913c64dffa25934a8d
Tomek <tomek-dziewulski@wp.pl>
2021-05-25 17:07:47 +0200

    6.1 Added kodilla-testing module

--- kodilla -testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla -testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,29 +1,8 @@
 package com.kodilla.testing;
-import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
-    public static void main (String [] args ){
-
-        Calculator calculator = new Calculator();
-        int liczba1=3;
-        int liczba2=4;
-        int liczba3=10;
-        int liczba4=4;
-        // test dodawania liczb
-        int wynik = calculator.add(liczba1,liczba2);
-        if(wynik ==(liczba1+liczba2))
-            System.out.println("Wynik  dodawania liczb:"+liczba1+" i "+liczba2+" jest prawidłowy i wynosi "+wynik);
-        else
-            System.out.println("Wynik  dodawania liczb:"+liczba1+" i "+liczba2+" jest nieprawidłowy i wynosi "+wynik);
-
-        // test odejmowania liczb
-        wynik=calculator.subtract (liczba3,liczba4);
-        if(wynik==(liczba3-liczba4))
-            System.out.println("Wynik  odejmowania liczb:"+liczba3+" i "+liczba4+" jest prawidłowy i wynosi "+wynik);
-        else
-            System.out.println("Wynik  odejmowania liczb:"+liczba3+" i "+liczba4+" jest nieprawidłowy i wynosi "+wynik);
-
-
-
+    public static void main (String [] args ) {
+        System.out.println("Modół 6 wprowadzenie do testowania oprogramowania");
     }
 }
